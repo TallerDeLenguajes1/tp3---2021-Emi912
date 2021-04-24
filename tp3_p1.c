d6490e3a657609f19e4da1d5c2d9ea1d34300d29
Emilio Merchan <emimerchan83@gmail.com>
2021-04-24 19:21:17 -0300

    Avance del punto falta corregir

--- tp3_p1.c
+++ tp3_p1.c
@@ -24,6 +24,8 @@ typedef struct Cliente
 void cargarCliente(Cliente *cliente, int id);
 void cargarProducto(Producto *producto, int cantProduct);
 int costoTotal(Producto *producto);
+void mostrarClientes(Cliente *cliente, int indice);
+void mostrarProductos(Producto *producto, int cantProduct);
 
 int main(){
     
@@ -41,7 +43,16 @@ int main(){
     {
         cargarCliente(clientes, i);
     }
+
+
+    for (int i = 0; i < cantClientes; i++)
+    {
+         mostrarClientes(clientes, i);
+    }
     
+       
+    
+
     return 0;
 }
 
@@ -65,7 +76,7 @@ void cargarCliente(Cliente *cliente, int id){
 void cargarProducto(Producto *producto, int cantProduct){
     for (int i = 0; i < cantProduct; i++)
     {
-        (producto + i)->ProductoID = i;
+        (producto + i)->ProductoID = i + 1;
         (producto + i)->Cantidad = rand() % 10 + 1;
         (producto + i)->TipoProducto = TiposProductos[rand() % 3 + 1];
         (producto + i)->PrecioUnitario = rand() % 91 + 10;
@@ -76,5 +87,33 @@ void cargarProducto(Producto *producto, int cantProduct){
 int costoTotal(Producto *producto){
 
     return (producto->Cantidad * producto->PrecioUnitario);
+
+}
+
+void mostrarClientes(Cliente *cliente, int indice){
+    printf("-----MOSTRANDO [%d] CLIENTE--------------\n", indice + 1);
+    printf("ID Cliente = %d \n", indice + 1);
+    printf("Nombre del cliente = %s\n", (cliente + indice)->NombreCliente);
+    printf("Cantidad de productos que lleva = %d\n", (cliente + indice)->CantidadProductosAPedir);
+    mostrarProductos((cliente + indice)->Productos, (cliente + indice)->CantidadProductosAPedir);
+    int costoNeto;
+    costoNeto = costoTotal((cliente + indice)->Productos);
+    printf("COSTO TOTAL %d", costoNeto);
+}
+
+void mostrarProductos(Producto *producto, int cantProduct){
+    printf("\n-----MOSTRANDO PRODUCTOS--------\n");
+    for (int i = 0; i < cantProduct; i++)
+    {
+        printf("\nMOSTRANDO PRODUCTO [%d]\n", i + 1);
+        printf("ID del producto = %d \n", (producto + i)->ProductoID);
+        printf("Cantidad de productos = %d \n",(producto + i)->Cantidad);
+        printf("Tipo de producto = %s \n",(producto + i)->TipoProducto);
+        printf("Precio unitario del producto = %f \n",(producto + i)->PrecioUnitario);
+    }
+    
+
+
+
 
 }
